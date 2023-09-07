afc6e1f8309d4c836e6bc32d19e47a15d6729c39
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-09-07 21:53:22 +0100

    get crypto info

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url('./images/error.avif')`
         alert('Something went wrong, please refresh the page')
     })
+
+fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById('crypto').innerText = data.name + " - " + data.market_data.current_price.gbp + " £"
+        document.getElementById('crypto').style.backgroundImage = `url('${data.image.small}')`
+    })
+    .catch(err => console.error(err))
