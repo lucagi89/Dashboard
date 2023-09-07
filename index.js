b6b61f1e8712a5c92513489334818d405c621994
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-09-07 21:01:50 +0100

    add features

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
     .then(res => res.json())
     .then(data => 
-        {console.log(data.urls.full)
-        document.querySelector('body').style.backgroundImage = `url(${data.urls.full})`
+        {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+        document.getElementById('author').textContent = `By: ${data.user.name}`
         }
         )
+    .catch(err =>{
+        console.log(err)
+        document.body.style.backgroundImage = `url('./images/error.avif')`
+        alert('Something went wrong, please refresh the page')
+    })
