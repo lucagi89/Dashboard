4eb0ccbfa90a9a2e9c4cca900d53b7b43a11c442
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-09-08 22:16:28 +0100

    weather full data

--- index.js
+++ index.js
@@ -63,18 +63,19 @@ setInterval(function(){
  async function success(pos) {
     const myWeatherKey = 'a286a3b6b4c2b61d8d57c51d29aa5e7f'
     const crd = pos.coords;
-    console.log(pos)
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&units=metric&appid=${myWeatherKey}`)
         .then(res => {
             if(!res.ok){
                 throw Error('Weather data not available')
             }
             return res.json()})
-        .then(data =>
-        document.getElementById('weather').innerHTML +=`
-                <img src='https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'/>`
-        
-        )
+        .then(data =>{
+        console.log(data)
+        document.getElementById('weather').innerHTML =`
+                <img src='https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'/>
+                <p>${data.name}</p>
+                <p>${Math.round(data.main.temp)}°</p>`
+        })
         .catch(err => console.error(err))
   }
   
